77a181fa3ee17c2616630edc5892e214121fffe0
Allen John <allenjohnmonapallil@gmail.com>
2024-06-16 21:03:29 +0530

    authentication,route and port updated

--- rent-here-client/src/config/axios.js
+++ rent-here-client/src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:10000',
+    // baseURL: 'http://localhost:10000',
+    baseURL: 'https://mern-rent-here.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
